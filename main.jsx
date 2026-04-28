6f44cfdfe7b7afab4bc78140c309bd1dddb18749
barttoni52-cloud <barttoni52@gmail.com>
2026-04-28 16:59:30 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import BartFit from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BartFit />
-  </StrictMode>,
-)
